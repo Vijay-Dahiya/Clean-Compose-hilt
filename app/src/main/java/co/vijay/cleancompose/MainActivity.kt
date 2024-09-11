a54139f61c2b8c202f15f972c910d5a1ffd97daa
vijay <vijaydahiyano.1@gmail.com>
2024-09-11 19:24:54 +0530

    row-box

--- app/src/main/java/co/vijay/cleancompose/MainActivity.kt
+++ app/src/main/java/co/vijay/cleancompose/MainActivity.kt
@@ -3,44 +3,56 @@ package co.vijay.cleancompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.vijay.cleancompose.ui.theme.CleanComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CleanComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            PreviewFun()
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PreviewFun() {
+    Column {
+        ListViewItem(imageId = R.drawable.ic_launcher_background, name = "John Doe", occupation = "Software Developer")
+        ListViewItem(imageId = R.drawable.ic_launcher_background, name = "John Doe", occupation = "Software Developer")
+        ListViewItem(imageId = R.drawable.ic_launcher_background, name = "John Doe", occupation = "Software Developer")
+        ListViewItem(imageId = R.drawable.ic_launcher_background, name = "John Doe", occupation = "Software Developer")
+
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CleanComposeTheme {
-        Greeting("Android")
+fun ListViewItem(imageId : Int, name : String, occupation : String) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(painter = painterResource(id = imageId), contentDescription ="", modifier = Modifier.size(40.dp) )
+        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+            Text(text = name, fontWeight = FontWeight.Bold)
+            Text(text = occupation, fontWeight = FontWeight.Thin)
+        }
     }
 }
