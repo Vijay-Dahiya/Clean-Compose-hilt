de1aac08486004ad6f027be91c7856d4c900a053
vijay <vijaydahiyano.1@gmail.com>
2024-09-14 19:51:27 +0530

    remember-saveable

--- app/src/main/java/co/vijay/cleancompose/MainActivity.kt
+++ app/src/main/java/co/vijay/cleancompose/MainActivity.kt
@@ -32,7 +32,8 @@ class MainActivity : ComponentActivity() {
         setContent {
 //            PreviewFun()
 
-            RecyclerViewCategory()
+//            RecyclerViewCategory()
+            NotificationScreen()
         }
     }
 }
